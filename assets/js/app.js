a8a015c86af5f7b14ccf0caca22438202fd776b1
unknown <mattcmej@gmail.com>
2018-09-26 09:08:22 -0700

    Add link in index.html to js file and to jquery, and type out app plan in app.js.

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+/* 
+
+Game Plan:
+
+Settings section
+    * Settings will be a form.
+    * Explanation of timing system will be displayed at the top of settings form.
+
+Timer section
+    * Timer will be displayed at top of screen, settings at bottom of screen.
+    * Clicking the "pause" buttons on the screen will change the button to "resume."
+    * Clicking "resume" will change the button to "pause."
+    * Number of "counted breaks" will be displayed above the timer.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
