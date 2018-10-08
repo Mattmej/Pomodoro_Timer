a6b79c0f8570b15bdbd19ea91f5625bae052ebdb
unknown <mattcmej@gmail.com>
2018-10-08 13:38:51 -0700

    Add a 'demos' folder to test out validation.

---
+++ demos/validation/my_digit_form/app.js
@@ -0,0 +1,13 @@
+// just for the demos, avoids form submit
+// jQuery.validator.setDefaults({
+//     debug: true,
+//     success: "valid"
+//   });
+  $( "#myform" ).validate({
+    rules: {
+      field: {
+        required: true,
+        digits: true
+      }
+    }
+  });
